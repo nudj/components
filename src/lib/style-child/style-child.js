28bfefb60f45758bae5bf4ea94e527e9d435fa98
Nick Collings <nick@collingo.com>
2017-08-14 16:09:55 +0100

    Fix tests

--- src/lib/style-child/style-child.js
+++ src/lib/style-child/style-child.js
@@ -1,9 +1,10 @@
 const React = require('react')
 const PropTypes = require('prop-types')
+const get = require('lodash/get')
 
 const StyleChild = (props, context) => {
   const child = get(props, 'children[0]')
-  if (!child) new Error('Please provide a child to style')
+  if (!child) throw (new Error('Please provide a child to style'))
   return React.cloneElement(child, context)
 }
 StyleChild.contextTypes = {

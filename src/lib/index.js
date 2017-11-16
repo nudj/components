640df0bd19cd66623787de4e386f0c967a6c3200
Timothy Robinson <timmyrobby@gmail.com>
2017-11-16 10:13:49 +0000

    Add input-field and iinput to exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   StyleChild: require('./style-child'),
   EmailForm: require('./email-form'),
-  Table: require('./table')
+  Table: require('./table'),
+  Input: require('./input'),
+  InputField: require('./input-field')
 }

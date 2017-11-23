057c4b8279e83c59ac09844301ad6e8fb3dc1772
Richard Palmer <rich@rdjpalmer.com>
2017-11-23 15:44:24 +0000

    Add default styling to table component

--- src/lib/components/table/style.css.js
+++ src/lib/components/table/style.css.js
@@ -1,14 +1,31 @@
 const { css } = require('../../css')
+const colors = require('../../css/colors')
+const sizes = require('../../css/sizes')
+const { type, fontWeight } = require('../../css/typography')
+const { merge } = require('@nudj/library')
 
 const styles = {
-  root: {},
-  table: {},
+  root: type.regular,
   header: {},
   headerRow: {},
-  heading: {},
+  heading: {
+    color: colors.primary,
+    fontWeight: fontWeight.bold,
+    paddingBottom: sizes.smallI,
+    paddingLeft: sizes.smallIi,
+    paddingRight: sizes.smallIi,
+    paddingTop: sizes.smallI,
+    textAlign: 'left'
+  },
   body: {},
   row: {},
-  cell: {}
+  cell: merge(type.regular, {
+    color: colors.text,
+    paddingBottom: sizes.smallI,
+    paddingLeft: sizes.smallIi,
+    paddingRight: sizes.smallIi,
+    paddingTop: sizes.smallI
+  })
 }
 
 module.exports = css(styles)

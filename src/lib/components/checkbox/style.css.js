f971f6f3ab372884e4667ae997be7b86a51df336
Richard Palmer <rich@rdjpalmer.com>
2017-12-04 09:51:09 +0000

    Add defaulting styling to checkbox (#38)

--- src/lib/components/checkbox/style.css.js
+++ src/lib/components/checkbox/style.css.js
@@ -1,15 +1,44 @@
-const { StyleSheet } = require('../../css')
+const { merge } = require('@nudj/library')
 
-const stylesheet = StyleSheet.create({
-  checkbox: {},
-  label: {},
-  wrapper: {
+const { StyleSheet, colors, sizes, typography } = require('../../css')
+
+const styleSheet = StyleSheet.create({
+  root: {
     position: 'relative'
   },
-  root: {
+  input: {
     opacity: 0,
     position: 'absolute'
-  }
+  },
+  icon: {
+    width: '1.5em',
+    height: '1.5em',
+    display: 'inline-block',
+    verticalAlign: 'middle'
+  },
+  iconBox: {
+    stroke: colors.grey,
+    strokeWidth: '2px'
+  },
+  iconCheckmark: {
+    fill: colors.midRed,
+    transformOrigin: '50% 50%',
+    transition: 'transform 150ms, opacity 150ms'
+  },
+  iconCheckmarkUnchecked: {
+    opacity: 0,
+    transform: 'scale(0)'
+  },
+  iconCheckmarkChecked: {
+    opacity: 1,
+    transform: 'scale(1)'
+  },
+  label: merge(typography.type.regular, {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    color: colors.text,
+    paddingLeft: sizes.smallIi
+  })
 })
 
-module.exports = stylesheet
+module.exports = styleSheet

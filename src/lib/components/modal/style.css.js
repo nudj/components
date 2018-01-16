ee796bec795d811a7488380e23e69de69c4927c8
Nick Collings <nick@collingo.com>
2018-01-16 18:53:06 +0000

    Remove maxHeight from modal

--- src/lib/components/modal/style.css.js
+++ src/lib/components/modal/style.css.js
@@ -1,17 +1,17 @@
-const { StyleSheet, colors } = require('../../css')
+const { StyleSheet, colors, sizes } = require('../../css')
 
 const stylesheet = StyleSheet.create({
   root: {},
   card: {
     position: 'absolute',
     width: '100%',
-    height: '100%',
     maxWidth: '37.5rem',
-    maxHeight: '28.125rem',
+    maxHeight: `calc(100% - ${sizes.largeIii})`,
     left: '50%',
     top: '50%',
     transform: 'translate(-50%, -50%)',
-    outline: 0
+    outline: 0,
+    overflowY: 'scroll'
   },
   overlay: {
     backgroundColor: colors.overlay[50],

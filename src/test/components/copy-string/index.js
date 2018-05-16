cc5c8c281c5e227afd64764b74d15bc3fe0dfe4c
Timothy Robinson <timmyrobby@gmail.com>
2018-05-16 17:20:44 +0100

    minor: Add CopyString component

---
+++ src/test/components/copy-string/index.js
@@ -0,0 +1,20 @@
+/* global expect */
+/* eslint-env mocha */
+
+const React = require('react')
+const { shallow } = require('enzyme')
+
+const CopyString = require('../../../lib/components/copy-string')
+
+describe('CopyString', () => {
+  it('renders', () => {
+    shallow(<CopyString text='Copy me'>Copy</CopyString>)
+  })
+
+  describe('props', () => {
+    it('renders its children', () => {
+      const component = shallow(<CopyString text='Copy me'>Copy</CopyString>)
+      expect(component.contains('Copy')).to.be.true()
+    })
+  })
+})

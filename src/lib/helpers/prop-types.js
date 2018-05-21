725389400775e33b6829b5a940111190a5bd5726
Richard Palmer <rich@rdjpalmer.com>
2018-05-21 16:37:07 +0100

    Fix StylePropType emulating `isRequired`

--- src/lib/helpers/prop-types.js
+++ src/lib/helpers/prop-types.js
@@ -5,8 +5,8 @@ const isObject = require('lodash/isObject')
 const StylePropType = (props, propName, componentName) => {
   const value = props[propName]
 
-  if (!isArray(value) && !isObject(value)) {
-    return new Error(`Invalid prop ${propName} supplied to ${componentName}. Should be a style object or an array of styles object`)
+  if (value && (!isArray(value) && !isObject(value))) {
+    return new Error(`Invalid prop ${propName} supplied to ${componentName}. Should be a style object or an array of style objects. You passed ${JSON.stringify(value)}`)
   }
 }
 

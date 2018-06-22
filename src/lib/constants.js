a6e20c3d5769eb0aa36ec9e2728136c204d123c2
Timothy Robinson <timmyrobby@gmail.com>
2018-06-22 10:26:01 +0100

    minor: Add numerics-related `KEY_CODES` to constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,7 +1,16 @@
 const FS_SHOW = false
 const FS_HIDE_CLASS = 'fs-hide'
 
+const KEY_CODES = {
+  E: 69,
+  PLUS: 187,
+  MINUS: 189,
+  FULL_STOP: 190,
+  ZERO: 48
+}
+
 module.exports = {
   FS_SHOW,
-  FS_HIDE_CLASS
+  FS_HIDE_CLASS,
+  KEY_CODES
 }

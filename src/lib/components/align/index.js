c66c25e93cd7bf10d7643b33cf3c822da6c42342
Nick Collings <nick@collingo.com>
2018-07-24 14:59:03 +0100

    Add crazy logs to align

--- src/lib/components/align/index.js
+++ src/lib/components/align/index.js
@@ -1,9 +1,14 @@
 // @flow
+console.log('c', 21)
 const React = require('react')
+console.log('c', 22)
 
 const { FS_SHOW, FS_HIDE_CLASS } = require('../../constants')
+console.log('c', 23)
 const { css, mergeStyleSheets } = require('../../css')
+console.log('c', 24)
 const defaultStyleSheet = require('./style.css')
+console.log('c', 25)
 
 type StyleSheet = {
   root?: Object,
@@ -11,6 +16,7 @@ type StyleSheet = {
   right?: Object,
   large?: Object
 }
+console.log('c', 26)
 
 type AlignProps = {
   styleSheet: StyleSheet,
@@ -20,6 +26,7 @@ type AlignProps = {
   rest?: Array<mixed>,
   nonsensitive?: boolean
 }
+console.log('c', 27)
 
 const Align = (props: AlignProps) => {
   const {
@@ -43,11 +50,14 @@ const Align = (props: AlignProps) => {
     </div>
   )
 }
+console.log('c', 28)
 
 Align.defaultProps = {
   primarySide: 'left',
   styleSheet: {},
   nonsensitive: FS_SHOW
 }
+console.log('c', 29)
 
 module.exports = Align
+console.log('c', 210)

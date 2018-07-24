c20354d02cad4fa3205c4a9d5c5e73867c10c0d0
Nick Collings <nick@collingo.com>
2018-07-24 14:40:45 +0100

    Add crazy logs to index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,46 +1,138 @@
+console.log('c', 0)
+const Align = require('./components/align')
+console.log('c', 1)
+const AnimateHeight = require('./components/animate-height')
+console.log('c', 2)
+const Banner = require('./components/banner')
+console.log('c', 3)
+const Button = require('./components/button')
+console.log('c', 4)
+const ButtonContainer = require('./components/button-container')
+console.log('c', 5)
+const Card = require('./components/card')
+console.log('c', 6)
+const Checkbox = require('./components/checkbox')
+console.log('c', 7)
+const CheckboxGroup = require('./components/checkbox-group')
+console.log('c', 8)
+const ContactCard = require('./components/contact-card')
+console.log('c', 9)
+const CopyString = require('./components/copy-string')
+console.log('c', 10)
+const Dropzone = require('./components/dropzone')
+console.log('c', 11)
+const EmailButton = require('./components/email-button')
+console.log('c', 12)
+const FacebookButton = require('./components/facebook-button')
+console.log('c', 13)
+const Icon = require('./components/icon')
+console.log('c', 14)
+const IconButton = require('./components/icon-button')
+console.log('c', 15)
+const IconLink = require('./components/icon-link')
+console.log('c', 16)
+const Input = require('./components/input')
+console.log('c', 17)
+const IntegerInput = require('./components/integer-input')
+console.log('c', 18)
+const IconInput = require('./components/icon-input')
+console.log('c', 19)
+const InputField = require('./components/input-field')
+console.log('c', 20)
+const Link = require('./components/link')
+console.log('c', 21)
+const LinkedinButton = require('./components/linkedin-button')
+console.log('c', 22)
+const ListMultiSelect = require('./components/list-multiselect')
+console.log('c', 23)
+const Loader = require('./components/loader')
+console.log('c', 24)
+const MessengerButton = require('./components/messenger-button')
+console.log('c', 25)
+const Modal = require('./components/modal')
+console.log('c', 26)
+const Pill = require('./components/pill')
+console.log('c', 27)
+const PillGroup = require('./components/pill-group')
+console.log('c', 28)
+const RadioBlock = require('./components/radio-block')
+console.log('c', 29)
+const RadioButton = require('./components/radio-button')
+console.log('c', 30)
+const RadioBlockGroup = require('./components/radio-block-group')
+console.log('c', 31)
+const RadioGroup = require('./components/radio-group')
+console.log('c', 32)
+const ScreenReadable = require('./components/screen-readable')
+console.log('c', 33)
+const SegmentedControl = require('./components/segmented-control')
+console.log('c', 34)
+const Select = require('./components/select')
+console.log('c', 35)
+const SelectablePill = require('./components/selectable-pill')
+console.log('c', 36)
+const SelectablePillGroup = require('./components/selectable-pill-group')
+console.log('c', 37)
+const Statistic = require('./components/statistic')
+console.log('c', 38)
+const StyleChild = require('./components/style-child')
+console.log('c', 39)
+const Table = require('./components/table')
+console.log('c', 40)
+const Text = require('./components/text')
+console.log('c', 41)
+const Textarea = require('./components/textarea')
+console.log('c', 42)
+const TwitterButton = require('./components/twitter-button')
+console.log('c', 43)
+const WhatsappButton = require('./components/whatsapp-button')
+console.log('c', 44)
+
 module.exports = {
-  Align: require('./components/align'),
-  AnimateHeight: require('./components/animate-height'),
-  Banner: require('./components/banner'),
-  Button: require('./components/button'),
-  ButtonContainer: require('./components/button-container'),
-  Card: require('./components/card'),
-  Checkbox: require('./components/checkbox'),
-  CheckboxGroup: require('./components/checkbox-group'),
-  ContactCard: require('./components/contact-card'),
-  CopyString: require('./components/copy-string'),
-  Dropzone: require('./components/dropzone'),
-  EmailButton: require('./components/email-button'),
-  FacebookButton: require('./components/facebook-button'),
-  Icon: require('./components/icon'),
-  IconButton: require('./components/icon-button'),
-  IconLink: require('./components/icon-link'),
-  Input: require('./components/input'),
-  IntegerInput: require('./components/integer-input'),
-  IconInput: require('./components/icon-input'),
-  InputField: require('./components/input-field'),
-  Link: require('./components/link'),
-  LinkedinButton: require('./components/linkedin-button'),
-  ListMultiSelect: require('./components/list-multiselect'),
-  Loader: require('./components/loader'),
-  MessengerButton: require('./components/messenger-button'),
-  Modal: require('./components/modal'),
-  Pill: require('./components/pill'),
-  PillGroup: require('./components/pill-group'),
-  RadioBlock: require('./components/radio-block'),
-  RadioButton: require('./components/radio-button'),
-  RadioBlockGroup: require('./components/radio-block-group'),
-  RadioGroup: require('./components/radio-group'),
-  ScreenReadable: require('./components/screen-readable'),
-  SegmentedControl: require('./components/segmented-control'),
-  Select: require('./components/select'),
-  SelectablePill: require('./components/selectable-pill'),
-  SelectablePillGroup: require('./components/selectable-pill-group'),
-  Statistic: require('./components/statistic'),
-  StyleChild: require('./components/style-child'),
-  Table: require('./components/table'),
-  Text: require('./components/text'),
-  Textarea: require('./components/textarea'),
-  TwitterButton: require('./components/twitter-button'),
-  WhatsappButton: require('./components/whatsapp-button')
+  Align,
+  AnimateHeight,
+  Banner,
+  Button,
+  ButtonContainer,
+  Card,
+  Checkbox,
+  CheckboxGroup,
+  ContactCard,
+  CopyString,
+  Dropzone,
+  EmailButton,
+  FacebookButton,
+  Icon,
+  IconButton,
+  IconLink,
+  Input,
+  IntegerInput,
+  IconInput,
+  InputField,
+  Link,
+  LinkedinButton,
+  ListMultiSelect,
+  Loader,
+  MessengerButton,
+  Modal,
+  Pill,
+  PillGroup,
+  RadioBlock,
+  RadioButton,
+  RadioBlockGroup,
+  RadioGroup,
+  ScreenReadable,
+  SegmentedControl,
+  Select,
+  SelectablePill,
+  SelectablePillGroup,
+  Statistic,
+  StyleChild,
+  Table,
+  Text,
+  Textarea,
+  TwitterButton,
+  WhatsappButton
 }
+
+console.log('c', 45)

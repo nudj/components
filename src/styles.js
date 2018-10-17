ef23715cce8b3961472d4dcf0aeaa1d38459145b
Timothy Robinson <timmyrobby@gmail.com>
2018-10-17 09:58:58 +0100

    minor: Add top-level style helpers export

---
+++ src/styles.js
@@ -0,0 +1,27 @@
+const mss = require('./lib/css/modifiers.css')
+const {
+  aphrodite,
+  StyleSheet,
+  mergeStyleSheets,
+  mergeStyle,
+  typography,
+  colors,
+  sizes,
+  utilities,
+  animations,
+  css
+} = require('./lib/css')
+
+module.exports = {
+  aphrodite,
+  StyleSheet,
+  mergeStyleSheets,
+  mergeStyle,
+  typography,
+  colors,
+  sizes,
+  utilities,
+  animations,
+  css,
+  mss
+}
